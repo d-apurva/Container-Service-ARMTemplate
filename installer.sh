7e9a9066d5d14565cdec9e46b0731abc27599ebf
d-apurva <apurva.dhoble@click2cloud.net>
2018-02-22 17:24:36 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 echo "Your software installation script"
-docker login --username  dishawani --password momisbest@123
+
+auth="ZGlzaGF3YW5pOm1vbWlzYmVzdEAxMjM="
+
+cat > /home/dockm/.docker/config.json << EOF
+{
+        "auths": {
+                "https://index.docker.io/v1/": {
+                        "auth": "$auth"
+                }
+        },
+        "HttpHeaders": {
+                "User-Agent": "Docker-Client/18.02.0-ce (linux)"
+        }
+}
+EOF
+
+chmod 600 /home/dockm/.docker/config.json
+
 docker run -d -it -p 9000:9000  -v /home/userdata:/click2cloud-dockm/data --name click2cloud-dockm-s2i dishawani/dockm
 
