d5a5bf927676a9559df15d359fbb57b7dd18a3c6
d-apurva <apurva.dhoble@click2cloud.net>
2018-02-22 10:28:41 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Your software installation script"
+docker login --username  dishawani --password momisbest@123
 docker run -d -it -p 9000:9000  -v /home/userdata:/click2cloud-dockm/data --name click2cloud-dockm-s2i dishawani/dockm
 
