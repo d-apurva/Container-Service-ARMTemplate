1b813b4325f0c6096f84f5d6a949240eb5e36aab
d-apurva <apurva.dhoble@click2cloud.net>
2018-02-23 18:25:13 +0530

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "Your software installation script"
-docker run -d -it -p 9000:9000  -v /home/userdata:/click2cloud-dockm/data  --name click2cloud-dockm-s2i click2cloud/hello-world
-
-
+docker run -d -it -p 9000:9000  -v /home/userdata:/click2cloud-dockm/data --name click2cloud-dockm-s2i dishawani/dockm
